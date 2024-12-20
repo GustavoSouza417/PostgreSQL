4dcabe09576823377e855ecc9bf4eda100be54ae
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2024-12-19 22:56:10 -0300

    Permissões do usuário "Ferdinando" adicionadas

--- models/9_DCL1.sql
+++ models/9_DCL1.sql
@@ -14,5 +14,11 @@ REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM Gabrielzinho;
 REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM Gabrielzinho;
 REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM Gabrielzinho;
 
+-- permissões que o usuário "Ferdinando" possui
+GRANT CONNECT ON DATABASE escola TO Ferdinando;
+GRANT INSERT ON curso TO Ferdinando;
+GRANT DELETE ON curso TO Ferdinando;
+GRANT UPDATE ON curso TO Ferdinando;
+
 -- dá acesso total ao banco de dados ao usuário "Irineu"
 ALTER ROLE Irineu WITH SUPERUSER;
