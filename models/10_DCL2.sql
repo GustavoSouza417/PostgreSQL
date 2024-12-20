6ae88509e905a21a21ae86cb8d7c68d301eae9c2
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2024-12-20 09:22:41 -0300

    Criação de ROLEs

---
+++ models/10_DCL2.sql
@@ -0,0 +1,28 @@
+-- ROLEs são criadas para gerenciar permissões
+-- ROLEs são atribuídas para usuários
+
+-- criando os padrões de usuário
+CREATE ROLE administrador;
+CREATE ROLE tecnico;
+CREATE ROLE cliente;
+
+-- criando os usuários
+CREATE USER user1;
+CREATE USER user2;
+CREATE USER user3;
+CREATE USER user4;
+CREATE USER user5;
+CREATE USER user6;
+
+-- configuração do padrão de usuário "administrador"
+ALTER ROLE administrador WITH SUPERUSER;
+
+-- configuração do padrão de usuário "técnico"
+
+-- atribuindo as ROLEs aos usuários
+GRANT administrador TO user1;
+GRANT administrador TO user2;
+GRANT tecnico TO user3;
+GRANT tecnico TO user4;
+GRANT cliente TO user5;
+GRANT cliente TO user6;
